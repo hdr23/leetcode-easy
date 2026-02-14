2afd6016b54ecf0d30c92ff47bd86bb7c7099705
hdr23 <rojasdaniel94@gmail.com>
2026-02-14 06:05:35 +0000

    gcovr + coverage flag

--- build.sh
+++ build.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+COVERAGE=""
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --coverage)
+      COVERAGE="-DENABLE_COVERAGE=ON"
+      shift
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
+
 mkdir -p build
 cd build
 
 # Run CMake to generate build files in the current 'build' directory using the parent directory's CMakeLists.txt
-cmake ..
+cmake .. $COVERAGE
 
 # Build the project using the generated build files (this will compile the code)
 cmake --build .
